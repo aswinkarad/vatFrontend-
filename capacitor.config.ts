e8d3f9fa1a2f8edcc4c40b11113431d4dba35acd
unknown <hexpdevfltr@gmail.com>
2024-08-23 12:40:12 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VaTfr',
+  webDir: 'dist'
+};
+
+export default config;
